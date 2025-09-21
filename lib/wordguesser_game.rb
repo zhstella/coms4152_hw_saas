762499bf8e5a7931b6fb30b87f80d34e638a8ecc
Yujia Zhai <yz5133@columbia.edu>
2025-09-21 21:46:47 +0000

    Part 3 progress: Sinatra app connected

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,9 +3,46 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    if letter.nil? || letter.empty? || letter !~ /^[a-z]$/i
+      raise ArgumentError
+    end
+
+    letter = letter.downcase
+
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+
+    if @word.downcase.include?(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+
+    true
+  end
+
+  def word_with_guesses
+    @word.chars.map { |ch| @guesses.include?(ch.downcase) ? ch : '-' }.join
+  end
+
+  def check_win_or_lose
+    if word_with_guesses == @word
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
   end
 
   # You can test it by installing irb via $ gem install irb
